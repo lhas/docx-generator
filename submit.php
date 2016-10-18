dd7cda6bd17ccbadfedfebc3173df4f07781f780
lhas <luizhrqas@gmail.com>
2016-10-18 10:39:26 -0200

    PHPword instead of VSword

--- submit.php
+++ submit.php
@@ -1,12 +1,22 @@
 <?php
 
-require_once './vsword/VsWord.php';
-VsWord::autoLoad();
+require_once 'vendor/autoload.php';
 
-$doc = new VsWord(); 
-$parser = new HtmlParser($doc);
-$parser->parse($_POST['content']);
+function parse_HTML_to_docx($content) {
+  // Creates a new DOCX Object
+  $phpWord = new \PhpOffice\PhpWord\PhpWord();
 
-$doc->saveAs('htmlparser.docx'); 
+  // Add a empty section
+  $section = $phpWord->addSection();
+
+  // Add the HTML content to the empty section
+  \PhpOffice\PhpWord\Shared\Html::addHtml($section, $content);
+
+  // Saving the document as DOCX file
+  $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+  $objWriter->save('curriculum.docx');
+}
+
+parse_HTML_to_docx($_POST['content']);
 
 ?>
